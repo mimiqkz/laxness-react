52bcff4c573a61d760a06ba70447b6323d03f1d8
quynh <npquynhdo@gmail.com>
2018-05-13 11:37:29 +0000

    package.json

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -15,25 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
-=======
-import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
-
-export default class myapp extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Hello world!</Text>
-      </View>
-    )  
-  }
-
-}
-
-AppRegistry.registerComponent('myapp', () => myapp);
->>>>>>> 7cc6591e84dac5dbcb8164aaa3e5f5fc64821694
