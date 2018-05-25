dc9b2fb47b14bd92d139e3627527dd9ae0c8fe37
zurgur <harryfreyr@gmail.com>
2018-05-25 17:34:30 +0000

    open share dialouge

--- app/components/Share.js
+++ app/components/Share.js
@@ -11,37 +11,34 @@ export default class share extends React.Component {
     }
 
     shareImage() {
-        //Do the share image thing here
-        console.info('test');
+        const base64Data = this.state.snapshot;
+        Share.share({
+            message: 'deildu með öðrum',
+            url: `data:image/png;base64,` + base64Data,
+            type: 'image/png',
+            title: '',
+            }, {
+            // Android only:
+            dialogTitle: 'Deildu með öðrum',
+            // iOS only:
+            excludedActivityTypes: [
+                'com.apple.UIKit.activity.PostToTwitter'
+            ]
+        })
     }
 
-    async takeScreenShot() {
-        const targetPixelCount = 1080; // If you want full HD pictures
-        const pixelRatio = PixelRatio.get(); // The pixel ratio of the device
-        // pixels * pixelratio = targetPixelCount, so pixels = targetPixelCount / pixelRatio
-        const pixels = targetPixelCount / pixelRatio;
-
-        const result = await takeSnapshotAsync(this.imageContainer, {
-            result: 'file',
-            height: pixels,
-            width: pixels,
-            quality: 1,
-            format: 'png',
-        });
-        
-        this.setState({result});
-    }
     async capture() {
 
-    
         const snapshot = await Expo.takeSnapshotAsync(this.image, {
           format: 'jpg',
           quality: 1,
-          result: 'file',
+          result: 'base64',
           width: 402,
           height: 402
         });
-        console.log('whoa dude: ', snapshot);
+        this.setState({snapshot: snapshot});
+        // console.log('whoa dude: ', snapshot);
+        this.shareImage(this.snapshot);
       }
 
     async componentWillMount() {
@@ -64,44 +61,7 @@ export default class share extends React.Component {
 }
 
 
-// export async function tackeScreenShoot() {
-//     const targetPixelCount = 1080; // If you want full HD pictures
-//     const pixelRatio = PixelRatio.get(); // The pixel ratio of the device
-//     // pixels * pixelratio = targetPixelCount, so pixels = targetPixelCount / pixelRatio
-//     const pixels = targetPixelCount / pixelRatio;
-
-//     const result = await takeSnapshotAsync(this.imageContainer, {
-//     result: 'file',
-//     height: pixels,
-//     width: pixels,
-//     quality: 1,
-//     format: 'png',
-//     });
-//     console.info(result)
-// }
-
 //  function ShareImage(base64Data){
-//     let options = {
-//         url: `data:image/png;base64,` + base64Data,
-//         type: 'image/png',
-//         message: '',
-//         title: ''
-//     };
-//     Share.open(options).then(response => {
-//         console.info('testin sharing image')
-//     })
-//     /*Share.share({
-//         message: ICON_PLUS_BASE64,
-//         url: "data:image/png;base64,<base64_data>",
-//         data: uri,
-//         }, {
-//         data: uri,
-//         // Android only:
-//         dialogTitle: 'Deildu með öðrum',
-//         // iOS only:
-//         excludedActivityTypes: [
-//           'com.apple.UIKit.activity.PostToTwitter'
-//         ]
-//       }),*/
+//     
 
 // }
